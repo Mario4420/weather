5b1e668ffd2f96e400cd805685057d23170f082f
Mario Maliqi <maliqimario@gmail.com>
2021-05-19 23:02:57 +0200

    done lol

--- main.py
+++ main.py
@@ -8,24 +8,21 @@ class App:
         self.owm = OWM("81bd3f534d8625a691bdfa7310e72ebd") 
         self.mgr = self.owm.weather_manager()
         self.city = tk.Entry()
-        self.state = tk.Entry()
         self.confirm_button = tk.Button(text = "confirm", command = self.get_weather)  
-        self.result = tk.Label() 
+        self.result = tk.Label(text = "Enter City:") 
         self.main() 
 
     def main(self):
         self.city.pack()
-        self.state.pack()  
         self.confirm_button.pack()
         self.result.pack()  
         self.window.mainloop() 
 
     def get_weather(self):
-        city = self.city.get() 
-        state = self.state.get() 
-        observation = self.mgr.weather_at_place(city.strip() + "," + state.strip())
+        city = self.city.get().strip()  
+        observation = self.mgr.weather_at_place(city)
         w = observation.weather
-        self.result.config(text = str(w.temperature('celsius')["temp"]) + "°") 
+        self.result.config(text = city + ": " + str(w.temperature('celsius')["temp"]) + "°") 
 
 if __name__ == "__main__":
     App()
